7a308c233595fc1dd73870ed56194cff9a47446a
engineer <engineer@iphonedevblr606435961.local>
2014-11-21 03:43:13 -0800

    Initial Commit

---
+++ ArunPOC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArunPOC
+//
+//  Created by engineer on 21/11/14.
+//  Copyright (c) 2014 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArunPOC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArunPOC
+//
+//  Created by engineer on 21/11/14.
+//  Copyright (c) 2014 Arun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
